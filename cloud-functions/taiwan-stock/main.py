2f0b4ad4ac727babbd4e1feb033e892e3c2d484d
ziv_y_chang <ziv_y_chang@trendmicro.com>
2024-07-10 13:59:53 +0800

    init

---
+++ cloud-functions/taiwan-stock/main.py
@@ -0,0 +1,93 @@
+import requests
+
+from datetime import datetime
+
+from firebase_functions import scheduler_fn, logger
+from firebase_admin import initialize_app, credentials, firestore
+
+cred = credentials.Certificate('service-account.json')
+app = initialize_app(cred)
+
+db = firestore.client(app)
+
+
+def parse_float(float_str: str):
+    try:
+        return float(float_str.replace('"', ''))
+    except ValueError:
+        return 0
+
+
+def get_twse_stock_info() -> list[dict]:
+    link = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY_ALL'
+    response = requests.get(link)
+    data = response.json()
+    data = data["data"]
+    parsed_data = []
+    now = datetime.now()
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
+    for d in data:
+        if len(d) < 9:
+            continue
+        stock_ticker = d[0].replace('"', '').strip()
+        stock_name = d[1].replace('"', '').strip()
+        stock_price = parse_float(d[7].strip())
+        parsed_data.append({
+            "symbol": stock_ticker,
+            "alias": stock_name,
+            "price": stock_price,
+            "currency": "TWD",
+            "market": "TWSE",
+            "updated": now
+        })
+    return parsed_data
+
+
+def get_tpex_stock_info():
+
+    link = 'https://www.tpex.org.tw/openapi/v1/tpex_esb_latest_statistics'
+    response = requests.get(link)
+    data = response.json()
+    parsed_data = []
+    now = datetime.now()
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
+    for d in data:
+        if "SecuritiesCompanyCode" not in d:
+            continue
+        stock_ticker = d["SecuritiesCompanyCode"]
+        stock_name = d["CompanyName"] if "CompanyName" in d else ""
+        stock_price = parse_float(
+            d["LatestPrice"]) if "LatestPrice" in d else 0
+        parsed_data.append({
+            "symbol": stock_ticker,
+            "alias": stock_name,
+            "price": stock_price,
+            "currency": "TWD",
+            "market": "TPEX",
+            "updated": now
+        })
+    return parsed_data
+
+
+def __taiwan_stock_price_sync():
+    stocks = get_twse_stock_info()
+
+    logger.log(f"Updating {len(stocks)} twse stocks")
+    for stock in stocks:
+        id = f"{stock['symbol']}-TWSE"
+        db.collection(
+            'ticker-price').document(id).set(stock)
+
+    stocks = get_tpex_stock_info()
+    logger.log(f"Updating {len(stocks)} tpex stocks")
+    for stock in stocks:
+        id = f"{stock['symbol']}-TPEX"
+        db.collection(
+            'ticker-price').document(id).set(stock)
+
+
+@scheduler_fn.on_schedule(schedule="40 15 * * 1-5", timezone="Asia/Taipei", timeout_sec=1200)
+def taiwan_stock_price_sync(event):
+    logger.log("Taiwan stock during market scheduler is running")
+    __taiwan_stock_price_sync()
+    logger.log("Taiwan stock during market scheduler is done")
---
+++ cloud-functions/us-stock/main.py
@@ -0,0 +1,82 @@
+import requests
+import time
+
+from datetime import datetime
+
+from firebase_functions import scheduler_fn, logger
+from firebase_admin import initialize_app, credentials, firestore
+from firebase_functions.params import IntParam, StringParam
+
+cred = credentials.Certificate('service-account.json')
+app = initialize_app(cred)
+
+db = firestore.client(app)
+
+
+def __get_stock_info(fmp_api_key: str, exchange: str, update_time: str) -> list[dict]:
+    link = f"https://financialmodelingprep.com/api/v3/symbol/{exchange}?apikey={fmp_api_key}"
+    response = requests.get(link)
+    data = response.json()
+    parsed_data = []
+    for d in data:
+        if "symbol" not in d:
+            continue
+        stock_ticker = d["symbol"]
+        stock_name = d["name"] if "name" in d else ""
+        stock_price = d["price"] if "price" in d else 0
+        parsed_data.append({
+            "symbol": stock_ticker,
+            "alias": stock_name,
+            "price": stock_price,
+            "currency": "USD",
+            "market": exchange,
+            "updated": update_time
+        })
+    return parsed_data
+
+
+def __insert_stock_info(stocks: list[dict], exchange: str):
+    for stock in stocks:
+        symbol = stock["symbol"]
+        id = f"{symbol}-{exchange}"
+        db.collection('ticker-price').document(id).set(stock)
+
+
+def get_nyse_stock_info(fmp_api_key: str) -> list[dict]:
+    exchange = "NYSE"
+    now = datetime.now()
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
+    parsed_data = __get_stock_info(fmp_api_key, exchange, now)
+
+    __insert_stock_info(parsed_data, exchange)
+
+
+def get_nasdaq_stock_info(fmp_api_key: str) -> list[dict]:
+    exchange = "NASDAQ"
+    now = datetime.now()
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
+    parsed_data = __get_stock_info(fmp_api_key, exchange, now)
+
+    __insert_stock_info(parsed_data, exchange)
+
+
+def get_amex_stock_info(fmp_api_key: str) -> list[dict]:
+    exchange = "AMEX"
+    now = datetime.now()
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
+    parsed_data = __get_stock_info(fmp_api_key, exchange, now)
+
+    __insert_stock_info(parsed_data, exchange)
+
+
+@scheduler_fn.on_schedule(schedule="0 17 * * 1-5", timezone="America/New_York", timeout_sec=1500)
+def us_stock_price_sync(event):
+    logger.log("US stock during market scheduler is running")
+    FMP_API_KEY = StringParam("FMP_API_KEY")
+    logger.log(f"FMP_API_KEY len: {len(FMP_API_KEY.value)}")
+    get_amex_stock_info(FMP_API_KEY.value)
+    time.sleep(5)
+    get_nyse_stock_info(FMP_API_KEY.value)
+    time.sleep(5)
+    get_nasdaq_stock_info(FMP_API_KEY.value)
+    logger.log("US stock during market scheduler is done")
